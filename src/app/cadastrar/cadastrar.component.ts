f9ddf763644ec5f378e0232f835125ebc20ecd34
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-06-01 14:46:34 -0300

    Consumo de API - Login e Cadastro II - 01/06

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+
+  usuario: Usuario = new Usuario();
+  confirmarSenh: string;
+  tipoUsuario: string;
+
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    window.scroll(0, 0);
+  }
+
+  confirmSenha(e: any) {
+    this.confirmarSenh = e.target.value;
+  }
+
+  tipoUser(e: any) {
+    this.tipoUsuario = e.target.value;
+  }
+
+  cadastrar() {
+    this.usuario.tipo = this.tipoUsuario
+    if (this.usuario.senha !== this.confirmarSenh) {
+      alert('senha incorretas')
+    } else {
+
+      this.authService.cadastrar(this.usuario).subscribe({
+        next: (res: Usuario) => {
+          this.usuario = res
+          this.router.navigate(['/entrar'])
+          alert('Usuário cadastrado com sucesso')
+        }
+      })
+    }
   }
 
 }
--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { environment } from 'src/environments/environment.prod';
+
+import { UsuarioLogin } from '../model/UsuarioLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  usuarioLogin: UsuarioLogin = new UsuarioLogin();
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    window.scroll(0, 0)
+  }
+
+  entrar() {
+    this.authService.entrar(this.usuarioLogin).subscribe({
+      next: ((res: UsuarioLogin) => {
+        this.usuarioLogin = res;
+        environment.token = this.usuarioLogin.token;
+        environment.id = this.usuarioLogin.id;
+        environment.nome = this.usuarioLogin.nome;
+        environment.foto = this.usuarioLogin.foto;
+
+        console.log({ ...environment })
+
+        this.router.navigate(['/inicio']);
+      }),
+      error: err => {
+        if (err.status === 401) {
+          alert('Usuário ou senha incorretos')
+        }
+      }
+    })
   }
 
 }
